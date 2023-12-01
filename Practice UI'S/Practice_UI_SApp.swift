3aafe194955168006d2443062ce4500de945cb11
Justin Garner <charlottewebsolutions1@gmail.com>
2023-12-01 10:20:53 -0500

    Initial Commit

---
+++ Practice UI'S/Practice_UI_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_UI_SApp.swift
+//  Practice UI'S
+//
+//  Created by Justin Garner on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Practice_UI_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
