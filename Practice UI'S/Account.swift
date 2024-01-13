4c895103afa21814e3b04df784f75ce64bc991e9
Justin Garner <charlottewebsolutions1@gmail.com>
2024-01-13 14:38:23 -0500

    inital commit

---
+++ Practice UI'S/Account.swift
@@ -0,0 +1,8 @@
+//
+//  Register:Login.swift
+//  Practice UI'S
+//
+//  Created by Justin Garner on 12/2/23.
+//
+
+import Foundation
---
+++ Practice UI'S/Preview Content/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  Practice UI'S
+//
+//  Created by Justin Garner on 12/2/23.
+//
+
+import Foundation
